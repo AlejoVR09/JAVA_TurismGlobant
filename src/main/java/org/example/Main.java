db4a4a141aa5895a7d6a3af0f327dc845397813b
Yeferson Idarraga <yyefer98@gmail.com>
2023-07-11 11:19:13 -0500

    Creacion de algunas clases, clases como atributos

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,16 @@
 package org.example;
 
+import org.example.modelos.Local;
+import org.example.modelos.Ofert;
+import org.example.modelos.Person;
+import org.example.modelos.Reserve;
+
 public class Main {
     public static void main(String[] args) {
         //System.out.println("Hello world!");
-
-
+        Person Alejo=new Person();
+        Ofert ofert=new Ofert();
+        Reserve reserve=new Reserve();
+        Local local=new Local();
     }
 }
---
+++ src/main/java/org/example/modelos/Samurai.java
@@ -0,0 +1,13 @@
+package org.example.modelos;
+
+public class Samurai {
+
+    public String name;
+    public String katanaName;
+    public Integer power;
+    public Boolean status;
+
+    public Samurai() {
+
+    }
+}
